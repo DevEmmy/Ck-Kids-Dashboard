c80117aa2b66304b2d6786b3c171490f35a7b951
Folayimi <ridwanfolayimi@gmail.com>
2023-08-23 16:13:58 -0700

    configured global.css

--- src/Components/CoursesPage.jsx
+++ src/Components/CoursesPage.jsx
@@ -1,16 +1,18 @@
-import GemView from "@/AtomicComponents/GemView";
-import MainContentOnOverview from "@/AtomicComponents/MainContentOnOverview";
 import Nav from "@/AtomicComponents/Nav";
 import React from "react";
+import { ChevronUp } from "heroicons-react";
 
 const CoursesPage = () => {
   return (
     <>
       <Nav />
-      <div className="w-full flexbs gap-[1em] py-[2em] px-xpadding">
-        <div className="w-[70%] cflexss gap-[1em]">
-            <div>
-                
+      <div className="w-full flexbs gap-[1em] py-[2em] px-[5%]">
+        <div className="w-[70%] cflexss gap-[1em] font-[400] text-[0.9rem]">
+            <div className="flexmm gap-[2em]">
+                <div className="flexmm p-[1em] border-[2px] gap-[1em] rounded-xl">
+                    <p>Categories</p>
+                    <ChevronUp />
+                </div>
             </div>
         </div>
         <div className="w-[30%] cflexmm gap-[1em]">
