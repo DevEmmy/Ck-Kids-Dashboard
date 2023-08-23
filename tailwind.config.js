1d441c0d29a56f7eca80f931506c956f8f707545
Folayimi <ridwanfolayimi@gmail.com>
2023-08-23 15:01:45 -0700

    made some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/AtomicComponents/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/AtomicComponents/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      spacing:{
-        xPadding: "5%"
+      spacing: {
+        xPadding: "5%",
       },
       gridTemplateColumns: {
         "3to1": "3fr 1fr",
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors:{
+      colors: {
         primary1: "#F5AE1E",
-      primary2: "#00AC76",
-      primary3: "#8D67CE",
-      primary4: "#FE5972"
-      }
+        primary2: "#00AC76",
+        primary3: "#8D67CE",
+        primary4: "#FE5972",
+      },
     },
   },
   plugins: [],
-}
+};
