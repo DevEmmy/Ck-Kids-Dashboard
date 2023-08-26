94cf8cdc2916d85935888c0ab368487463f60f57
Folayimi <ridwanfolayimi@gmail.com>
2023-08-25 22:56:09 -0700

    Made some changes

---
+++ src/Components/Leadership.jsx
@@ -0,0 +1,14 @@
+import Nav from "@/AtomicComponents/Nav";
+
+const Leadership = () => {
+  return (
+    <>
+      <Nav active={3} />
+      <div className="w-full cflexss px-[5%] py-[2em]">
+        <h1>LEADERSHIP-BOARD</h1>
+      </div>
+    </>
+  );
+};
+
+export default Leadership;
