c509d87dea5c03e129c73697ebdcdff85c55abaf
Folayimi <ridwanfolayimi@gmail.com>
2023-09-09 10:52:01 +0100

    made some changes

--- src/Components/Teachers-Dashboard/Dashboard.jsx
+++ src/Components/Teachers-Dashboard/Dashboard.jsx
@@ -1,6 +1,15 @@
 import Overview from "./Overview";
-import { FiGrid } from "react-icons/fi";
-import { ChevronDown, ChevronUp } from "heroicons-react";
+import { useState, useEffect } from "react";
+import { FiGrid, FiBell, FiChevronDown } from "react-icons/fi";
+import {
+  ChevronDown,
+  ChevronUp,
+  UserCircleOutline,
+  CogOutline,
+  QuestionMarkCircleOutline,
+  LogoutOutline,
+} from "heroicons-react";
+import Notification from "./Notification";
 const Dashboard = () => {
   const Nav = [
     {
@@ -28,6 +37,36 @@ const Dashboard = () => {
       icon: <FiGrid size="16px" />,
     },
   ];
+
+  const profMenuItems = [
+    {
+      name: "Profile",
+      // link: "/profile",
+      icon: <UserCircleOutline color="black" size={20} />,
+    },
+    {
+      name: "Settings",
+      link: "/kids-dashboard-settings",
+      icon: <CogOutline color="black" size={20} />,
+    },
+    {
+      name: "Support",
+      link: "/kids-dashboard/support",
+      icon: <QuestionMarkCircleOutline color="black" size={20} />,
+    },
+    {
+      name: "Log Out",
+      link: "/signin",
+      icon: <LogoutOutline color="black" size={20} />,
+      action: () => {
+        localStorage.clear();
+      },
+    },
+  ];
+
+  const [profMenu, setProfMenu] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const [notification, setNotification] = useState(false);
   return (
     <>
       <div className="w-full flexss">
@@ -53,9 +92,78 @@ const Dashboard = () => {
         </div>
 
         <div className="w-[80%]">
-            <div className="w-full p-[37px] bg-white flexbs">
-
+          <div className="w-full p-[30px] font-[700] text-[28px] bg-white flexbs border-b-[1px]">
+            <p>Teachers Dashboard Overview</p>
+            <div className="flexsm gap-[20px]">
+              <div
+                className="cursor-pointer border rounded-[8px] p-[8px]"
+                onClick={() => {
+                  setNotification(true);
+                }}
+              >
+                <FiBell />
+              </div>
+              <div
+                className="relative flex gap-[7px] font-[600] items-center cursor-pointer text-[12px]"
+                onClick={() => {
+                  setProfMenu(!profMenu);
+                }}
+              >
+                <img
+                  src="/teacherAvatar.svg"
+                  className="w-[32px] h-[32px] object-cover rounded-md"
+                />
+                <p>Savannah Nguyen</p>
+                <FiChevronDown />
+                {profMenu && (
+                  <>
+                    <div className="absolute cflexss gap-[0.5em] top-[4em] right-xPadding w-[14em] bg-white rounded-xl shadow-md border-2 p-[0.5em] z-50">
+                      {profMenuItems.map((item, i) => {
+                        return (
+                          <>
+                            {item.name === "Profile" ? (
+                              <>
+                                <div
+                                  className="flex w-full items-center gap-[1em] p-[10px] rounded-lg hover:bg-primary2 cursor-pointer hover:text-white transition-all duration-500"
+                                  onClick={() => {
+                                    if (item.name === "Profile") {
+                                      setProfile(true);
+                                    }
+                                  }}
+                                >
+                                  <span className={`flexmm`}>{item.icon}</span>
+                                  <p className="text-[0.9em]">{item.name}</p>
+                                </div>
+                              </>
+                            ) : (
+                              <>
+                                {" "}
+                                <a
+                                  href={item.link}
+                                  className="w-full"
+                                  onClick={item.action && item.action}
+                                >
+                                  <div className="flex w-full items-center gap-[1em] p-[10px] rounded-lg hover:bg-primary2 cursor-pointer hover:text-white transition-all duration-500">
+                                    <span className={`flexmm`}>
+                                      {item.icon}
+                                    </span>
+                                    <p className="text-[0.9em]">{item.name}</p>
+                                  </div>
+                                </a>
+                              </>
+                            )}
+                          </>
+                        );
+                      })}
+                    </div>
+                  </>
+                )}
+                {notification && (
+                  <Notification setNotification={setNotification} />
+                )}
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </>
---
+++ src/Components/Teachers-Dashboard/Notification.jsx
@@ -0,0 +1,76 @@
+import { XCircleOutline } from 'heroicons-react';
+import React, { useState } from 'react'
+import { FaCrown } from 'react-icons/fa';
+
+const Notification = ({setNotification}) => {
+    const dummy = [
+        {
+            title: "Congratulations, You've Completed a Course",
+            details: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio officiis odio repudiandae vitae expedita sapiente sit consectetur quidem facere numquam.",
+            type: 1,
+        },
+        {
+            title: "New Course Alert: Game Development Pro!",
+            details: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio officiis odio repudiandae vitae expedita sapiente sit consectetur quidem facere numquam.",
+            type: 2,
+        }
+    ]
+  return (
+    <div className="absolute cflexss gap-[0.5em] top-[4em] right-xPadding w-[400px] bg-white rounded-xl shadow-md border-2 p-3 z-50 ">
+        <div className="w-full flexbm">
+            <h2 className="font-[900] text-[1.1rem]">Notifications</h2>
+            <XCircleOutline
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setNotification(false);
+              }}
+              color="gray"
+            />
+          </div>
+        
+        <div className='grid gap-4'>
+            {
+                dummy.map((data, i)=>{
+                    return(
+                       <NotificationModule data={data} key={i}/>
+                    )
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+const NotificationModule = ({data})=>{
+
+    const [showDetails, setShowDetails] = useState(false)
+    return(
+        <div className='flex gap-2 justify-between  items-start'>
+        <div className='p-5 bg-primary2 rounded-md text-white'>
+            <FaCrown />
+        </div>
+
+        <div className='w-[80%]'>
+            <p className='' onClick={()=> setShowDetails(!showDetails)}>
+                {
+                    data.title
+                }
+            </p>
+
+            {
+                showDetails && 
+                <p className='font-[400] text-gray-500'>
+                {
+                    data.details
+                }
+            </p>
+            }
+            
+        </div>
+
+        <div className='h-3 w-3 bg-primary2 rounded-full'/>
+    </div>
+    )
+}
+
+export default Notification
