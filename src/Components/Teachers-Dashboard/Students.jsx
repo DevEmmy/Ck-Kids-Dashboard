9ae1258e6c79d14f3ebfe3edab04ea0b3e651b5e
Folayimi <ridwanfolayimi@gmail.com>
2023-09-11 22:07:36 +0100

    completed student aspect of the teachers board

--- src/Components/Teachers-Dashboard/Students.jsx
+++ src/Components/Teachers-Dashboard/Students.jsx
@@ -3,18 +3,18 @@ import {
   ArrowLeft,
   ArrowRight,
   ChevronRight,
+  EyeOutline,
+  PencilAltOutline,
+  TrashOutline,
+  XCircleOutline
 } from "heroicons-react";
 import { useState } from "react";
-import { BsBadgeSd, BsFire, BsTrophy } from "react-icons/bs";
-import { IoDiamondOutline } from "react-icons/io5";
-import { GiGraduateCap } from "react-icons/gi";
-
-import {} from "react-icons/ri";
-import { Graduate } from "next/font/google";
+import StudentProfile from "./StudentProfile";
 
 const Students = () => {
   const [profile, setProfile] = useState(false);
   const [data, setData] = useState({});
+  const [trash, setTrash] = useState(false);
   const StudentData = [
     {
       checked: false,
@@ -110,13 +110,14 @@ const Students = () => {
   return (
     <>
       <div className="relative w-full p-[30px] cflexss gap-[28px]">
-        {profile && (
+        {profile && trash !== true && (
           <StudentProfile
             profile={profile}
             setProfile={setProfile}
             data={data}
           />
         )}
+        {trash && <Trash setTrash={setTrash} data={data} />}
         <div className="w-full flexes">
           <div className="flexmm gap-[10px] rounded-[8px] border-[1px] p-[16px] w-[526px] bg-white">
             <SearchOutline size="16px" />
@@ -180,7 +181,17 @@ const Students = () => {
                   <div className="w-[14%] flexsm">@{data.username}</div>
                   <div className="w-[14%] flexsm">{data.dateJoined}</div>
                   <div className="w-[14%] flexsm">{data.class}</div>
-                  <div className="w-[14%] flexsm">Actions</div>
+                  <div className="w-[14%] flexsm gap-[20px]">
+                    <EyeOutline size="16px" />
+                    <PencilAltOutline size="16px" />
+                    <TrashOutline
+                      size="16px"
+                      onClick={() => {
+                        setTrash(true);
+                        setData(data);
+                      }}
+                    />
+                  </div>
                 </div>
               </>
             );
@@ -204,91 +215,53 @@ const Students = () => {
 
 export default Students;
 
-const StudentProfile = ({ profile, setProfile, data }) => {
+const Trash = ({ setTrash, data }) => {
+  const [remove, setRemove] = useState(false);
   return (
     <>
-      <div
-        className={`absolute top-0 z-25 w-[35.75rem] bg-[#FFF] text-[0.75rem] lg:text-[0.6rem] font-[400] border-l-[1px] border-b-[1px] text-[#808080] px-[20px] ${
-          profile ? "right-0" : "right-[-100%]"
-        } py-[30px] flex-shrink cflexss gap-[15px] transition-all duration-1000`}
-      >
-        <div
-          className="p-[0.75em] flexmm border-[1px] rounded-[0.5rem] cursor-pointer"
-          onClick={() => {
-            setProfile(false);
-          }}
-        >
-          <ChevronRight size="16px" />
-        </div>
-        <div className="w-full flexmm">
-          <div className="cflexmm gap-[1.25em]">
-            <div className="relative w-[9.375em] h-[9.375em] rounded-full">
-              <img
-                src={`/${data.avatar}.svg`}
-                className="rounded-full"
-                alt="profileAvatar"
-              />
-              <div className="absolute bottom-0 right-0 bg-primary2 w-[1.5625rem] h-[1.5625rem] border-[4px] border-white rounded-full" />
-            </div>
-            <div className="cflexmm gap-[0.25em]">
-              <p className="font-[700] text-[1.25rem] lg:text-[1.1rem]">
-                {data.name}
-              </p>
-              <p>@{data.username}</p>
-              <p>{data.email}</p>
-              <div className="border-[1px] py-[0.25rem] px-[0.625rem] flexmm rounded-[0.25rem]">
-                {data.class}
+      <div className="fixed w-full h-full top-0 left-0 bg-secondary1 flexmm">
+        <div className="relative cflexmm gap-[30px] w-[580px] h-[341px] bg-[#FAFAFA] border-[1px] rounded-[12px] flex-shrink m-[20px] shadow-md">
+          {remove ? (
+            <>
+              <div className="w-[286px] text-center font-[600] text-[24px]">
+                <p>Wait for approval from the principal</p>
               </div>
-            </div>
-            <div className="w-full grid grid-cols-2 gap-[20px]">
-              <div className="w-[252px] h-[130px] rounded-[0.75rem] p-[2px] pb-[8px] text-primary3 bg-primary3">
-                <div className="bg-white rounded-t-[0.75rem] h-full rounded-b-[0.25rem] flexmm gap-[16px]">
-                  <IoDiamondOutline size="2.5rem" />
-                  <div className="cflexsm gap-[0.125rem]">
-                    <p>Learning Gems</p>
-                    <p className="font-marker text-[1.625rem] lg:text-[1.425rem] ls:text-[1.2rem]">
-                      50,000 LG
-                    </p>
-                  </div>
-                </div>
+              <div className="absolute top-[8%] right-[8%] flexmm cursor-pointer" onClick={()=>{
+                setTrash(false)                
+              }}>
+                <XCircleOutline size="16px" />
               </div>
-              <div className="w-[252px] h-[130px] rounded-[12px] pb-[8px] text-white bg-[#A37414]">
-                <div className="bg-primary1 w-full rounded-t-[12px] h-full pl-[20px] rounded-b-[0.25rem] cflexms gap-[8px]">
-                  <div className="flexmm gap-[13px]">
-                    <div className="p-[10px] rounded-[6.498px] text-primary1 bg-white">
-                      <BsFire size="23px" />
-                    </div>
-                    <p className="font-[800] text-[40px] lg:text-[36px] ls:text-[30px]">200</p>
-                  </div>
-                  <p className="text-[#222] text-[16px] lg:text-[14px] ls:text-[12px]">Day Streak</p>
-                </div>
+            </>
+          ) : (
+            <>
+              <div className="w-full cflexmm text-[24px] lg:text-[20px] font-[400]">
+                <p className="font-[800]">Are You Sure?</p>
+                <p>You are about to delete</p>
+                <p>
+                  <span className="text-primary2 font-[700]">{data.name}</span>{" "}
+                  profile
+                </p>
               </div>
-              <div className="w-[15.75rem] h-[8.125rem] rounded-[0.75rem] pb-[8px] text-white bg-[#00734F]">
-                <div className="bg-primary2  w-full rounded-t-[12px] h-full pl-[20px] rounded-b-[0.25rem] cflexms gap-[8px]">
-                  <div className="flexmm gap-[13px]">
-                    <div className="p-[10px] rounded-[6.498px] text-primary2 bg-white">
-                      <GiGraduateCap size="23px" />
-                    </div>
-                    <p className="font-[800] text-[40px] lg:text-[36px] ls:text-[30px]">100</p>
-                  </div>
-                  <p className="text-[#FFF] text-[16px] lg:text-[14px] ls:text-[12px]">Completed Courses</p>
-                </div>
+              <div className="w-full flexmm gap-[20px] font-[600] text-[19px] lg:text-[17px]">
+                <button
+                  className="py-[18px] px-[60px] text-[white] bg-[#F00] rounded-full border-none"
+                  onClick={() => {
+                    setRemove(true);
+                  }}
+                >
+                  Delete
+                </button>
+                <button
+                  className="py-[18px] px-[60px] rounded-full text-[#808080] border-[1px]"
+                  onClick={() => {
+                    setTrash(false);
+                  }}
+                >
+                  Cancel
+                </button>
               </div>
-              <div className="w-[15.75rem] h-[8.125rem] rounded-[0.75rem] pb-[8px] text-white bg-[#5E4589]">
-                <div className="bg-primary3 rounded-t-[0.75rem] h-full rounded-b-[0.25rem] pl-[20px] flexsm gap-[13px]">
-                  <div className="p-[10px] rounded-[6.498px] text-primary3 bg-white">
-                    <BsTrophy size="23px" />
-                  </div>
-                  <div className="cflexmm">
-                    <p className="text-[#FFF] text-[16px] lg:text-[14px] ls:text-[12px]">Mastery Stages</p>
-                    <p className="font-[800] text-[26px] lg:text-[24px] ls:text-[20px]">
-                      Innovator 
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            </>
+          )}
         </div>
       </div>
     </>
