26e30727ac3fb8522785ec65586db1d67b71dc57
Folayimi <ridwanfolayimi@gmail.com>
2023-09-18 21:05:28 +0100

    made some changes

--- src/Components/Schools-Dashboard/UploadStudentData.jsx
+++ src/Components/Schools-Dashboard/UploadStudentData.jsx
@@ -8,12 +8,12 @@ const UploadStudentData = ({ close }) => {
   const [fileName, setFileName] = useState("");
   const [fileError, setFileError] = useState(false);
   const [loading, setLoading] = useState(false);
-  
+
   const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileChange = (e) => {
     setSelectedFile(e.target.files[0]);
-    console.log(e.target.files[0])
+    console.log(e.target.files[0]);
   };
 
   const upload = (file) => {
@@ -39,12 +39,12 @@ const UploadStudentData = ({ close }) => {
     e.preventDefault();
 
     const formData = new FormData();
-    formData.append('file', selectedFile);
+    formData.append("file", selectedFile);
     // if (file) {
     setLoading(true);
-      // console.log(file)
-    await uploadData(formData)
-    close()
+    // console.log(file)
+    await uploadData(formData);
+    close();
     // }
   };
 
@@ -58,6 +58,12 @@ const UploadStudentData = ({ close }) => {
       <div className="w-[526px] flex-shrink cflexss gap-[20px]">
         <div className="w-full cflexss gap-[12px]">
           <p className="font-[800]">Upload Student Data</p>
+          <p className="w-full py-[5px] font-[400]">
+            Download a sample excel sheet{" "}
+            <span className="font-[600] text-primary2 cursor-pointer underline">
+              <a href="/">here</a>
+            </span>
+          </p>
           <div className="w-full flexmm text-[#808080] text-[16px] border-[2px] border-[#808080] border-dotted rounded-[12px] h-[125px] lg:text-[14px] font-[400] cursor-pointer">
             {file ? fileName.name : <p>Drop excel file to upload here</p>}
             <div className="absolute opacity-0">
@@ -70,7 +76,7 @@ const UploadStudentData = ({ close }) => {
                   setFileName(base64);
                 }}
               /> */}
-              <input type="file" onChange={handleFileChange} name="file"/>
+              <input type="file" onChange={handleFileChange} name="file" />
             </div>
           </div>
           {fileError && (
--- src/app/schools-dashboard/page.jsx
+++ src/app/schools-dashboard/page.jsx
@@ -1,30 +1,31 @@
-'use client'
+"use client";
 
-import Dashboard from '@/Components/Schools-Dashboard/Dashboard'
-import { fetchFromLS } from '@/services/request'
-import { notifyError } from '@/services/toastify'
-import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import Dashboard from "@/Components/Schools-Dashboard/Dashboard";
+import { fetchFromLS } from "@/services/request";
+import { notifyError } from "@/services/toastify";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
+  const router = useRouter();
 
-  const router = useRouter()
-
-  const [school, setSchool] = useState()
+  const [school, setSchool] = useState();
 
   useEffect(() => {
     let data = fetchFromLS("school")
     setSchool(data);
 
-    // if(!data){
-    //   router.push("/signin")
-    //   notifyError("UnAuthorized")
-    // }
-  }, []); 
+    if(!data){
+      router.push("/signin")
+      notifyError("UnAuthorized")
+    }
+  }, []);
 
   return (
-    <Dashboard school={school}/>
-  )
-}
+    <>
+      <Dashboard school={school} />          
+    </>
+  );
+};
 
-export default page
+export default page;
