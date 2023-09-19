a3af75d63bdeecfc03adb1ba6fe1de5386ec3e05
Folayimi <ridwanfolayimi@gmail.com>
2023-09-19 21:44:14 +0100

    made some changes

--- src/Components/Teachers-Dashboard/EachCourse.jsx
+++ src/Components/Teachers-Dashboard/EachCourse.jsx
@@ -20,8 +20,8 @@ const EachCourse = ({ video, setCat, setType, setAdd, setDrop, fetchData }) => {
   return (
     <div className="w-full rounded-[12px] border-[1px] flex">
       <div className="w-3/5 border-r-[1px] p-[15px] flex gap-[20px] justify-between">
-        <div className="flex gap-5 ">
-          <div className="flexmm w-full min-h-[200px] flex-shrink rounded-[12px]">
+        <div className="w-full flex gap-5 ">
+          <div className="flexmm w-[100%] h-[250px] flex-shrink rounded-[12px]">
             <img
               src={video.cover}
               alt="courseImage"
