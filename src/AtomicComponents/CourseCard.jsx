d21b910928bb6931484311da1e0ab24189399b65
DevEmmy <eolaosebikan60@gmail.com>
2023-10-01 21:06:04 +0100

    adjusted course card ui

--- src/AtomicComponents/CourseCard.jsx
+++ src/AtomicComponents/CourseCard.jsx
@@ -16,7 +16,7 @@ const CourseCard = ({
         href={`/kids-dashboard/courses/${_id}`}
         className="min-w-[300px] h-[400px]"
       >
-        <div className="w-full hover:border-primary2 border-[1px] border-white cflexss gap-[20px] p-2 rounded-[24px] cursor-pointer shadow-lg">
+        <div className="w-auto hover:border-primary2 border-[1px] border-white cflexss gap-[20px] p-2 rounded-[24px] cursor-pointer shadow-lg">
           <div className="w-full flexmm h-[218px] lg:h-[190px]">
             <img
               src={cover}
@@ -25,15 +25,15 @@ const CourseCard = ({
             />
           </div>
 
-          <div className="px-2">
+          <div className="px-2 w-full">
               <div className="py-[2px] px-[10px] bg-[#F9F5FF] text-purplePrime text-[14px] rounded-full font-[400] w-auto">
                 {category}
               </div>
-              <div className="w-full cflexss my-2">
-                <p className="text-[20px] lg:text-[18px] lf:text-[20px] font-[700] text-[#101828]">
+              <div className=" cflexss my-2">
+                <p className="text-[20px] line-clamp-1 w-full lg:text-[18px] lf:text-[20px] font-[700] text-[#101828]">
                   {name}
                 </p>
-                <p className="text-[16px] font-[400] text-[#667085] line-clamp-3">
+                <p className="text-[16px] font-[400] text-[#667085] line-clamp-3 w-full">
                   {description}
                 </p>
                 {/* <div className="flexsm gap-[16px] py-[10px]">
