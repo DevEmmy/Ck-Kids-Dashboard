462c16756d41ef98b166139f5d9755e934ecd23e
Folayimi <ridwanfolayimi@gmail.com>
2023-10-13 15:32:55 -0700

    made

--- src/app/admin-signin/page.jsx
+++ src/app/admin-signin/page.jsx
@@ -1,12 +1,13 @@
 "use client";
-import AdminSignin from "@/Components/AdminSignin";
+// import AdminSignin from "@/Components/AdminSignin";
 
 // import Price from "@/Components/Price";
 
 export default function Home() {
   return (
     <>
-      <AdminSignin />
+      {/* <AdminSignin /> */}
+      <h1>Admin SigninL</h1>
     </>
   );
 }
