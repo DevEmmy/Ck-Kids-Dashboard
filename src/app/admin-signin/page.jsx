cdf7a4b90269ed238992b3bfd22ba7bc84e5f99f
Folayimi <ridwanfolayimi@gmail.com>
2023-10-13 14:43:19 -0700

    made changes

---
+++ src/app/admin-signin/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import AdminSignIn from "@/Components/AdminSignIn";
+
+const adminLogin = () => {
+  return (
+    <>
+      <AdminSignIn />
+    </>
+  );
+};
+
+export default adminLogin;
