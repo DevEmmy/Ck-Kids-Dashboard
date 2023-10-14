f48c1419bda438f545cb587ed96b5ed8206a3e5c
Folayimi <ridwanfolayimi@gmail.com>
2023-10-14 20:47:37 +0100

    made changes

--- src/app/set-password/page.jsx
+++ src/app/set-password/page.jsx
@@ -1,28 +1,27 @@
 "use client";
 import SetPassword from "@/Components/SetPassword";
-import { useRouter } from "next/router";
+import { useSearchParams, usePathname } from "next/navigation";
 
-export default function Home() { 
-  const router = useRouter()
+export default function Home() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const url = `${pathname}?${searchParams}`;
+
   function parseURLParameters(url) {
     const params = {};
-    const searchParams = url.split('?')[1];
+    const searchParams = url.split("?")[1];
     if (searchParams) {
-      searchParams.split('&').forEach(param => {
-        const [key, value] = param.split('=');
+      searchParams.split("&").forEach((param) => {
+        const [key, value] = param.split("=");
         params[key] = value;
       });
     }
     return params;
   }
-  
-  const currentURL = router.asPath;
-  const parameters = parseURLParameters(currentURL);
-  
-  const token = parameters['token'];
-  const type = parameters['type'];
-  
-  return (
+  const token = parseURLParameters(url)["token"];
+  const type = parseURLParameters(url)["type"];    
+
+  return ( 
     <>
       <SetPassword token={token} type={parseInt(type)} />      
     </>
